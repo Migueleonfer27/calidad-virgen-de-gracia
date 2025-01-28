7e9676d5f5f34e16338feb9cc6c074356cce5414
Jaime <jaime.ornu@gmail.com>
2025-01-28 13:46:11 +0100

    Arreglado y mejorado logout, empezado el recuperar contraseña.

--- back/routes/auth-routes.js
+++ back/routes/auth-routes.js
@@ -1,8 +1,10 @@
 // Jaime Ortega
 import { Router } from 'express'
 import { AuthController } from '../controllers/auth-controller.js'
+import { isIdIntMiddleware, emailMiddleware, passwordMiddleware } from '../middlewares/user-middleware.js'
+import { validateJWT } from '../middlewares/auth-middleware.js'
 
 export const router = Router()
 
 router.post('/login', AuthController.login)
-router.post('/logout', AuthController.logout)
+router.post('/logout', [validateJWT], AuthController.logout)
