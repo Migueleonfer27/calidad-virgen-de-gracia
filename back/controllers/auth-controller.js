75903c0290fc26420237ca645ed1d66521431218
Jaime <jaime.ornu@gmail.com>
2025-01-28 10:52:49 +0100

    Creado logout, que usa revokeUserTokens. Creado middleware que valida token. Creada ruta de logout.

--- back/controllers/auth-controller.js
+++ back/controllers/auth-controller.js
@@ -3,10 +3,16 @@ import { response, request } from "express"
 import bcrypt from 'bcrypt'
 import { AuthConnection } from "../databases/auth-connection/auth-connection.js"
 import { generateJWT_roles } from "../helpers/generate-jwt.js"
+import jwt from 'jsonwebtoken'
 
-const authConnection = new AuthConnection()
+export const authConnection = new AuthConnection()
+
+let blacklistedTokens = []
 
 const AuthController = {
+    revokeUserTokens: (token) => {
+        blacklistedTokens.push(token)
+    },
     login: async (req = request, res = response) => {
         const { email, password } = req.body
         try {
@@ -21,8 +27,17 @@ const AuthController = {
             }
 
             const roles = await authConnection.getUserRoles(user.id)
-            const token = await generateJWT_roles(user.email, roles)
+            const token = await generateJWT_roles(user.id, roles)
             return res.status(200).json({ message: 'Login exitoso', user, token })
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
+    },
+    logout: async (req = request, res = response) => {
+        const user = req.params.id
+        try {
+            AuthController.revokeUserTokens(user)
+            return res.status(200).json({ message: 'Logout exitoso' })
         } catch (error) {
             return res.status(500).json({ message: error.message })
         }
