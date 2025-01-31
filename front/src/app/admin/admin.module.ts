0f873e0438eed5f976826f8507edcc2f978a6a55
Gema Rubio <gemarubio89@gmail.com>
2025-01-31 09:42:04 +0100

    limpieza carpetas

--- front/src/app/admin/admin.module.ts
+++ front/src/app/admin/admin.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminPageComponent } from './pages/components/admin-page/admin-page.component';
-import { CategoriesComponent } from '../category/components/categoriesTable/categories-table.component';
-
+import { NgModule } from '@angular/core';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { CategoryModule } from '../category/category.module';
-import { MatButtonModule } from '@angular/material/button';
-
 
 @NgModule({
   declarations: [
     AdminPageComponent,
-
-
   ],
   imports: [
     CommonModule,
