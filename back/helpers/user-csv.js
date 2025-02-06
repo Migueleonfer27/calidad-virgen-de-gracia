31a342a22c8f7f39c858ea68c81f4d7bcf6c83ac
Migueleonfer27 <94015029+Migueleonfer27@users.noreply.github.com>
2025-02-06 09:58:16 +0100

    Arreglo csv back

--- back/helpers/user-csv.js
+++ back/helpers/user-csv.js
@@ -1,17 +1,28 @@
 import fs from "fs";
 import csv from "csv-parser";
 import User from "../models/user.js";
-
+import bcrypt from "bcrypt";
 
 const importUsersFromCSV = async (filePath) => {
   return new Promise((resolve, reject) => {
     const users = [];
+    const hashPromises = [];
 
     fs.createReadStream(filePath)
       .pipe(csv({ separator: ";" }))
-      .on("data", (data) => users.push(data))
+      .on("data", (data) => {
+        if (data.leave_date === "0000-00-00" || data.leave_date === "") data.leave_date = null;
+
+        const hashPromise = bcrypt.hash(data.password, 10).then((hashedPassword) => {
+          data.password = hashedPassword;
+          users.push(data);
+        });
+
+        hashPromises.push(hashPromise);
+      })
       .on("end", async () => {
         try {
+          await Promise.all(hashPromises);
           await User.bulkCreate(users, { ignoreDuplicates: true });
           fs.unlinkSync(filePath);
           resolve({ users });
