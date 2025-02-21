617fb1bcb8c6fa45ea81a1eb0a8e62d4574bb651
Gema Rubio <gemarubio89@gmail.com>
2025-02-21 20:48:45 +0100

    añado middleware para abilities

---
+++ back/middlewares/abilities-mddleware.js
@@ -0,0 +1,22 @@
+export const isRolValid = (permiso) => {
+    return async (req, res, next) => {
+      try {
+        //0 no tiene token
+        //1 no tiene las abilities adecuadas
+        //2 problemas con la consulta de las abilities
+        const id_rol = req.user?.roleId; 
+        if (!id_rol) {
+          return res.status(401).json({ cod: 0 });
+        }
+  
+        const abilities = await connection_rol.getAbilities(id_rol);
+        if (abilities.includes(permiso)) {
+          return next();
+        }
+  
+        return res.status(403).json({ cod:1 });
+      } catch (error) {
+        return res.status(500).json({ cod:2 });
+      }
+    };
+  };
