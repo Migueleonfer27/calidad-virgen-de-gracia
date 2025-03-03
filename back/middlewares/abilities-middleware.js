48b0737e3d397fb9c783a55bfd725e926cdc4001
Gema Rubio <gemarubio89@gmail.com>
2025-03-04 00:06:22 +0100

    cambio isRolValid() en el back por si a Jaime no le da tiempo a cambiar lo del token para que siga funcionando así

--- back/middlewares/abilities-middleware.js
+++ back/middlewares/abilities-middleware.js
@@ -11,14 +11,23 @@ export const isRolValid = (permiso) => {
         //0 no tiene las abilities adecuadas
         //2 problemas con la consulta de las abilities
         const rol = req.user.roles; 
-        console.log(rol)
+        
         if (!rol) {
           return res.status(401).json({ cod: 0, msg: messages.abilitiesMiddleware.roleless});
         }
+        let allAbilities=[]
+        for (const element of rol) {
+          const abilities = await getAbilities(element.role_id);
+          allAbilities.push(abilities.abilities);
+        }
+        
+        const abilityRequired= allAbilities.flat().filter(ability =>ability.description == permiso)
+      
+        /*Comentar y descomentar si Jaime arregla el token
         const abilities= await getAbilities(rol[0].role_id)
        
         const abilityRequired= abilities.abilities.filter(ability =>ability.dataValues.description == permiso) 
-      
+      */
         if (abilityRequired.length > 0) {
         
           return next()
--- front/src/app/shared/components/header/header.component.ts
+++ front/src/app/shared/components/header/header.component.ts
@@ -30,7 +30,7 @@ export class HeaderComponent {
     abilities.deleteTask,
     abilities.updateTask,
     abilities.createTask,
-    abilities.updateStateTask,
+    
   ];
   public abiltiesAdmin= [
     abilities.getUsers,
